d84017c5f580ea6f42ad9659d5b22005918c7077
Zhi Fan <zhifan.chen@studenti.unimi.it>
2020-11-01 16:50:45 +0100

    Updated

---
+++ 2/L02-codice/code/cesare.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+        char testo [1024];
+        int c = 0;
+        int i = 0;
+
+        while ((c = getchar()) != '.') {
+                testo[i] = c;
+                i++;
+        };
+        testo[i] = '\0';
+
+        scanf("%d", &c);
+
+        for(i = 0; testo[i] != '\0'; i++) {
+                printf("%c", testo[i] + c);
+        }
+
+        return 0;
+}
+//comportamento strano
--- 2/L02-codice/code/divisioni.c
+++ 2/L02-codice/code/divisioni.c
@@ -5,10 +5,15 @@ int main( void) {
     int b, n, i = 0;
     int c[N] = {0}; 
     
+    //[4]
+    printf("Inserisci un numero da convertire ");
     scanf( "%d %d", &n, &b );
     
     do c[i++] = n % b;
         while ( ( n /= b ) > 0 );
+
+    //[4]
+    printf("Il numero convertito è: ");
 
     while ( i > 0 )
         printf( "%d", c[--i] ); 
@@ -21,3 +26,8 @@ int main( void) {
 /*
  * [1] n -> numero da convertire
  *     b -> base d'arrivo del numero da convertire
+ * [2] cifre
+ * [3] conversione in base b
+ * [5] Il programma converte un numero n in una qualunque base b che deve essere minore di 10
+ * [6] È la terza cifra più significativa del numero n
+ */
