b5dd7aad3dd1071e16c8f212ee80e916fb2815dd
Zhi Fan <zhifan.chen@studenti.unimi.it>
2020-11-05 11:04:59 +0100

    3 completato

--- 6/lista.c
+++ 6/lista.c
@@ -12,12 +12,74 @@ Node newNode( int val, Node p );
 int f1 ( Node p );
 Node f2 ( Node p );
 Node f3 ( Node p, Node q );
+void list_print(Node l);
+void list_printInv(Node l);
+int * listToArray(Node l, int size);
+int listSize(Node list);
+void list_destroy(Node l);
+void list_destroy_iter(Node l);
 
-int main(){
-	Node x = newNode( 1, NULL );
-	Node head = newNode( 2, x );
+int main(){ Node x = newNode( 1, NULL ); Node head = newNode( 2, x );
+        head = newNode(3, head);
+        
+        list_print(head);
+        printf("Fai schifo\n");
+        list_printInv(head);
+        int size = listSize(head);
+        int * array = listToArray(head, size);
+
+        for (int i = 0; i < size; i++) {
+                printf("%d ", array[i]);
+        }
+
+        printf("%c\n", 8);
+
+//        list_destroy(head);
+        list_destroy_iter(head);
 }
 
+void list_print(Node l) { //3.1
+        if (l == NULL) {
+                return;
+        }
+
+        printf("%p %d\n",l, l->val);        
+        
+        if (l->next != NULL) {
+                list_print(l->next);
+        }
+}
+
+void list_printInv(Node l) { //3.2
+        if (l == NULL) {
+                return;
+        }
+
+        if (l->next == NULL) {
+                printf("p: %p, val: %d\n", l, l->val);
+                return;
+        }
+
+        list_printInv(l->next);
+        printf("p: %p, val: %d\n", l, l->val);
+}
+
+int listSize(Node list) {
+        int i = 0;
+        for (; list != NULL; i++, list = list->next);
+
+        return i;
+}
+
+int * listToArray(Node l, int size) {
+        int * array = (int *) malloc(sizeof(int) * size);
+        
+        for (int i = 0; l != NULL; i++, l = l->next) {
+                array[i] = l->val;
+        };
+
+        return array;
+}
 
 Node newNode( int val, Node p ){
 	Node new = malloc( sizeof (struct node));
@@ -59,4 +121,19 @@ Node f3 ( Node p, Node q ){
 	
 }
 
+void list_destroy(Node l) {
+        if (l == NULL) {
+                return;
+        }
 
+        list_destroy(l->next);
+        free(l);
+}
+
+void list_destroy_iter(Node l) {
+        for (Node next = l->next; l != NULL && next->next != NULL; l = next, next = next->next) {
+                free(l);
+        }
+}
+
+//[5] è void perché non deve ritornare nulla dato che la lista viene cancellata
