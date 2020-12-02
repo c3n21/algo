29f12ca95211d8a5c610d76b94062f38b19da0fb
Zhi Fan <zhifan.chen@studenti.unimi.it>
2020-12-02 17:28:15 +0100

    Impleted pqueue

--- 9/src/pqueue/pqueue.c
+++ 9/src/pqueue/pqueue.c
@@ -12,7 +12,7 @@ struct pqueue {
 
 void heapify_up(Pqueue queue, int last);
 
-void heapify_down(Pqueue queue);
+void heapify_down(Pqueue queue, int current);
 
 /* crea una coda di priorità vuota che potrà contenere al massimo n Item */
 Pqueue pqueue_new (int n) {
@@ -39,16 +39,24 @@ int pqueue_length (Pqueue queue) {
 };
 
 /* inserisce l’Item nella coda di priorità */
-void pqueue_insert ( Pqueue queue, Item item) {
+void pqueue_insert (Pqueue queue, Item item) {
         if (queue->count < queue->size) {
                 queue->h[queue->count] = item;
                 heapify_up(queue, queue->count++);
         }
 };
 /* estrae dalla coda di priorità l’Item con chiave minima */
-Item pqueue_extractmin ( Pqueue );
+Item pqueue_extractmin (Pqueue queue) {
+        Item last = queue->h[1];
+        if (queue->count > 0) {
+                queue->count--;
+        }
+        return last;
+}
 /* restituisce l’Item con chiave minima nella coda di priorità */
-Item pqueue_min ( Pqueue );
+Item pqueue_min (Pqueue queue) {
+        return queue->h[1];
+}
 
 void heapify_up(Pqueue queue, int current) {
         if (current > 1) {
@@ -64,5 +72,17 @@ void heapify_up(Pqueue queue, int current) {
         }
 }
 
-void heapify_down(Pqueue queue) {
+void heapify_down(Pqueue queue, int current) {
+        if (2*current <= queue->size) {
+                int left  = 2*current;
+                int right = (right = (2*current) + 1) <= queue->size? right : left; //no figlio destro
+
+                if (item_compare(queue->h[left], queue->h[right]) == -1) { //left è più piccolo di destro
+                        queue->h[current] = queue->h[left];
+                        heapify_down(queue, left);
+                } else { //left è più grande o uguale
+                        queue->h[current] = queue->h[right];
+                        heapify_down(queue, right);
+                }
+        }
 }
